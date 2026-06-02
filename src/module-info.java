d0aa8e36d5844dd84f14a0b9a4b177bd6f5ba970
profevertonpires <everton.pires@pro.ucsal.br>
2026-06-02 19:52:24 -0300

    projeto
    wrwerwerwer

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author everton.pires
+ *
+ */
+module projeto_tes {
+}
---
+++ src/projeto_tes/Mainha.java
@@ -0,0 +1,10 @@
+package projeto_tes;
+
+public class Mainha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
